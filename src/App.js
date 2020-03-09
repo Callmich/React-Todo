7c15dd1cc682e1656f14f181cbe7604662967cc1
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-09 13:54:10 -0600

    added info in app that has been passed down into 2 child components. currently working on rentering the toDO items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import ToDoList from './components/TodoList';
+import ToDoForm from './components/TodoForm'
+
+const toDo = [
+  {
+    task: "Work out",
+    id: 1,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+    toDo: toDo,
+    task: ""
+    };
+  }
+
+  toggleToDo = clickedItemId => {
+    this.setState({
+      toDo: this.state.toDo.map(item => {
+        if (item.id === clickedItemId){
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  addTask = taskName => {
+    const newTask = {
+      task: taskName,
+      id: new Date(),
+      completed: false
+    };
+    this.setState({
+      toDo: [...this.state.toDo, newTask]
+    });
+  }
+  
+
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoForm 
+          addTask={this.addTask}
+        />
+        <ToDoList 
+          toDo={this.state.toDo}
+          toggleToDo={this.toggleToDo}
+        />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+
+import React from 'react';
+
+import Todo from './Todo';
+
+const ToDoList = props => {
+    return (
+        <div>
+            {props.toDo.map(item => (
+                <Todo 
+                    key={item.id}
+                    item={item}
+                    toggleToDo={props.toggleToDo}
+                />
+            ))}
+            <button onClick={props.clearToDo}>
+                Clear Finished Tasks
+            </button>
+        </div>
+    )
+}
