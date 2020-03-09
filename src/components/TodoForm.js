d3418dc9fafac4128f9bac93388b5faa5ef9db28
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-09 14:08:36 -0600

    functionality added to create items for ToDOList

---
+++ src/components/TodoForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class ToDoForm extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            item: ''
+        }
+    }
+
+    handleChanges = e => {
+        this.setState({
+            item: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTask(this.state.item);
+        this.setState({
+            item: ''
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    name="item"
+                    value={this.state.item}
+                    onChange={this.handleChanges}
+                />
+                <button>Add</button>
+            </form>
+        )
+    }
+
+}
+
+export default ToDoForm
